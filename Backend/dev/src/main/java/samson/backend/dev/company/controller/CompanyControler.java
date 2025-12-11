a8a3bba12600cd269c1569c7c0760e7ec818b210
Phạm Huy <huyphamquang2003@gmail.com>
2025-12-11 19:50:07 +0700

    Add company controller

--- Backend/dev/src/main/java/samson/backend/dev/company/controller/CompanyControler.java
+++ Backend/dev/src/main/java/samson/backend/dev/company/controller/CompanyControler.java
@@ -1,14 +1,30 @@
 package samson.backend.dev.company.controller;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import samson.backend.dev.company.model.CompanyModel;
+import samson.backend.dev.company.service.CompanyService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@RequestMapping("/api/company")
 public class CompanyControler {
-    @GetMapping("/")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping("path")
+    public List<CompanyModel> getAllCompany() {
+        return companyService.getAllCompanies();
+    }
+    @PostMapping("path")
+    public CompanyModel createCompany(@RequestBody CompanyModel company) {
+        return companyService.createCompany(company);
     }
     
 }
--- Backend/dev/src/main/java/samson/backend/dev/company/service/CompanyService.java
+++ Backend/dev/src/main/java/samson/backend/dev/company/service/CompanyService.java
@@ -1,10 +1,29 @@
 package samson.backend.dev.company.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import samson.backend.dev.company.API.CompanyExternalInterface;
 import samson.backend.dev.company.API.CompanyInternalInterface;
+import samson.backend.dev.company.model.CompanyModel;
+import samson.backend.dev.company.repository.CompanyRepo;
 
-@Service
-class CompanyService implements CompanyExternalInterface, CompanyInternalInterface{
+// add more service for interface later
+@Service 
+public class CompanyService implements CompanyExternalInterface, CompanyInternalInterface{
+    @Autowired
+    private final CompanyRepo companyRepo;
+
+    public CompanyService(CompanyRepo companyRepo) {
+        this.companyRepo = companyRepo;
+    }
+
+    public List<CompanyModel> getAllCompanies() {
+        return companyRepo.findAll();
+    }
     
+    public CompanyModel createCompany(CompanyModel company) {
+        return companyRepo.save(company);
+    }
 }   
