a3aa2ead628511a35924d40d9f253e52262b0812
NhatVo2971 <vopdnhat.work@gmail.com>
2025-12-15 15:53:31 +0700

    create mockNotication.js for notification

---
+++ Samson_JM_M2/Samson_JM_FE/job_manager_fe/src/modules/company/data/mockNotification.js
@@ -0,0 +1,18 @@
+const mockNotifications = [
+  {
+    id: 1,
+    title: "New applicant applied",
+    message: "Nguyen Van A applied for Senior Frontend Developer.",
+    time: "2 mins ago",
+    unread: true,
+  },
+  {
+    id: 2,
+    title: "Interview reminder",
+    message: "You have an interview schedule tomorrow 9:00 AM.",
+    time: "1 hour ago",
+    unread: false,
+  },
+];
+
+export default mockNotifications;
