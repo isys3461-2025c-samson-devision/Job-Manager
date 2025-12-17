b0abf180592fe2b64ac32341e8a5fcae545300bd
Duy To <s3979932@rmit.edu.vn>
2025-12-17 21:13:49 +0700

    Changed port in CORS to match frontend

--- Backend/dev/src/main/java/samson/backend/dev/backend_config/CorsConfig.java
+++ Backend/dev/src/main/java/samson/backend/dev/backend_config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001") //Change port number later to match front end
+                .allowedOrigins("http://localhost:3000") //Frontend port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
