b9fc26e4c536b35acad5d8d923f6d403f946fca7
NhatVo2971 <vopdnhat.work@gmail.com>
2026-01-09 17:25:30 +0700

    Update CompanyDashboard.jsx

--- Samson_JM_M2/Samson_JM_FE/job_manager_fe/src/modules/company/pages/CompanyDashboard.jsx
+++ Samson_JM_M2/Samson_JM_FE/job_manager_fe/src/modules/company/pages/CompanyDashboard.jsx
@@ -166,7 +166,6 @@ export default function CompanyDashboard() {
           filterStatus={filterStatus}
           setFilterStatus={setFilterStatus}
         />
-
         {/* =======================
             JOB LIST
         ======================== */}
